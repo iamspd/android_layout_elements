9678e9e515f18da2daebaac73944f0e812ade493
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-18 17:12:33 -0500

    notifications implemented

--- app/src/main/java/com/example/androidlayoutelements/MainActivity.java
+++ app/src/main/java/com/example/androidlayoutelements/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.androidlayoutelements;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static com.example.androidlayoutelements.App.CHANNEL_1_ID;
+import static com.example.androidlayoutelements.App.CHANNEL_2_ID;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.Notification;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NotificationManagerCompat notificationManager;
+    private EditText mEditTitle, mEditMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationManager = NotificationManagerCompat.from(this);
+
+        mEditTitle = findViewById(R.id.etTitle);
+        mEditMessage = findViewById(R.id.etMessage);
+    }
+
+    public void onChannel1Clicked(View view) {
+
+        String title = mEditTitle.getText().toString();
+        String message = mEditMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationManagerCompat.IMPORTANCE_HIGH)
+                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .build();
+
+        notificationManager.notify(1, notification);
+    }
+
+    public void onChannel2Clicked(View view) {
+
+        String title = mEditTitle.getText().toString();
+        String message = mEditMessage.getText().toString();
+
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_2_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationManagerCompat.IMPORTANCE_LOW)
+                .build();
+
+        notificationManager.notify(2, notification);
     }
 }
