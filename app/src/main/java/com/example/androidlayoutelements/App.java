74ec0758cac22ea3533bf9bacd45b7c5a10af5d3
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-18 16:14:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/androidlayoutelements/App.java
@@ -0,0 +1,41 @@
+package com.example.androidlayoutelements;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application {
+
+    public static final String CHANNEL_1_ID = "Channel1";
+    public static final String CHANNEL_2_ID = "Channel2";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Hi",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is Channel1");
+
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Hello",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel2.setDescription("This is Channel2");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
+        }
+    }
+}
