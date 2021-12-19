7dbd6eaa8d3b76a5a43b6c3af36774462a440bd3
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2021-12-18 22:51:07 -0500

    notifications style implemented

--- app/src/main/java/com/example/androidlayoutelements/MainActivity.java
+++ app/src/main/java/com/example/androidlayoutelements/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.app.NotificationManagerCompat;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -37,16 +39,25 @@ public class MainActivity extends AppCompatActivity {
         String message = mEditMessage.getText().toString();
 
         Intent i = new Intent(this, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,
+                0, i, PendingIntent.FLAG_IMMUTABLE);
 
         Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
         broadcastIntent.putExtra("message", message);
         PendingIntent pendingBroadcast = PendingIntent.getBroadcast(this,
                 0, broadcastIntent, PendingIntent.FLAG_IMMUTABLE);
 
+        // decode image to bitmap
+        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_1_ID)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.ic_icon)
                 .setContentTitle(title)
+                .setLargeIcon(largeIcon)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Hi There!")
+                        .setBigContentTitle("Hello")
+                        .setSummaryText("Summary"))
                 .setContentText(message)
                 .setPriority(NotificationManagerCompat.IMPORTANCE_HIGH)
                 .setCategory(NotificationCompat.CATEGORY_MESSAGE)
@@ -69,6 +80,17 @@ public class MainActivity extends AppCompatActivity {
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(title)
                 .setContentText(message)
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("This is line 1")
+                        .addLine("This is line 2")
+                        .addLine("This is line 3")
+                        .addLine("This is line 4")
+                        .addLine("This is line 5")
+                        .addLine("This is line 6")
+                        .addLine("This is line 7")
+                        .setBigContentTitle("Hello")
+                        .setSummaryText("Summary")
+                )
                 .setPriority(NotificationManagerCompat.IMPORTANCE_LOW)
                 .build();
 
